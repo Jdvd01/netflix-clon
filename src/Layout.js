52a00e0c36bebab947f93bd68185a2f0d613e5a2
Jose Velasquez <velasquezdenorajose@gmail.com>
2022-08-30 18:50:50 -0400

    Create app with create-react-app && install react-router-dom

---
+++ src/Layout.js
@@ -0,0 +1,11 @@
+import Prueba from "./components/Prueba/Prueba.jsx" 
+
+function Layout() {
+  return (
+    <>
+      <Prueba />
+    </>
+  );
+}
+
+export default Layout;
--- src/components/Prueba/Prueba.jsx
+++ src/components/Prueba/Prueba.jsx
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
-import './App.css';
+import './Layout.css';
 
-function App() {
+function Layout() {
   return (
     <div className="App">
       <header className="App-header">
@@ -22,4 +22,4 @@ function App() {
   );
 }
 
-export default App;
+export default Layout;
